297eef4ce2ecf77dbb8318b52e51f696ce41505f
Keyoonz <89485044+Keyoonz@users.noreply.github.com>
2024-09-24 12:38:20 +0200

    keycode comment

--- include/input.h
+++ include/input.h
@@ -6,6 +6,18 @@
 #include "terminal.h"
 #include <unistd.h>
 
+// KEYS keycode
+//
+// ARROW_UP \x1b[A
+// ARROW_DOWN \x1b[B
+// ARROW_RIGHT \x1b[C
+// ARROW_LEFT \x1b[D
+// PAGE_UP \x1b[5~
+// PAGE_DOWN \x1b[6~
+// HOME \x1b[1~ || <esc>[7~ || <esc>[H  || <esc>OH
+// END \x1b[4~ || <esc>[8~ || <esc>[F  || <esc>OF
+// DELETE \x1b[3~
+
 void editorMoveCursor(struct editorConfig *E, int key);
 
 /**
